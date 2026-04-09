2872387bc84e5f66fb50e1fcf42a722ef7947098
marcospaula-marBR <marcos.paula@marbr.com.br>
2026-04-09 17:29:31 -0300

    fix(supabase): update fallback api key to fix invalid api key error in vercel

--- dashboard-v2/src/lib/supabase.ts
+++ dashboard-v2/src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ngtjhwswbbivqajtpjvg.supabase.co';
 
-const ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndGpod3N3YmJpdnFhanRwanZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwNTc4NjMsImV4cCI6MjA2My41NTc4NjN9.y7z6r4lLqx-qC3Y5X5QwQ7XqX3Yw8z5LqyY3Lg';
+const ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndGpod3N3YmJpdnFhanRwanZnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2OTE4MjM2MCwiZXhwIjoyMDg0NzU4MzYwfQ.2TPnOfnAzeWG23Y-VuDKxxzQ9QdbHwrnHdVBhS9hU28';
 
 const getSupabaseKey = () => {
   if (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
