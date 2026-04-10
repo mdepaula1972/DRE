ab910f46a58a45cec3a14fcce89e5e468e963b15
marcospaula-marBR <marcos.paula@marbr.com.br>
2026-04-10 11:37:57 -0300

    fix(build): remove temp debug scripts causing tsc errors

--- dashboard-v2/scripts/test_db.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from '../src/lib/supabase';
-
-async function test() {
-  const { data } = await supabase.from('omie_cp_titulos').select('*').limit(2);
-  console.log("CP Keys:", Object.keys(data[0]));
-}
-
-test().catch(console.error);
